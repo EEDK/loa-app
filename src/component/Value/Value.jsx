cd088edb3a91059cf94a66c235fd47e52db73a13
KDE <kde9604@naver.com>
2023-02-28 18:11:54 +0900

    add MUI and Jest

--- src/component/Value/Value.jsx
+++ src/component/Value/Value.jsx
@@ -1,20 +1,37 @@
 import React from "react";
 import "./Value.css";
 import PropTypes from "prop-types";
-
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemText from "@mui/material/ListItemText";
+import Divider from "@mui/material/Divider";
 
 // 값을 도출하는 각각의 컴포넌트
-function Value({name, value}){
-    return (
-      <div >
-        <h1>{`${name}의 현재 값은 ${value}입니다.`}</h1>
-      </div>
-    )
+function Value({ name, value }) {
+  return (
+    <div>
+      <List
+        sx={{
+          width: "100%",
+          maxWidth: 500,
+          bgcolor: "background.paper",
+        }}
+      >
+        <ListItem>
+          <ListItemText
+            primary={`${name}의 현재 값은 ${value} 골드 입니다.`}
+            secondary="Jan 9, 2014"
+          />
+        </ListItem>
+        <Divider variant="inset" component="li" />
+      </List>
+    </div>
+  );
 }
 
 Value.propTypes = {
-    value: PropTypes.number.isRequired,
-    name: PropTypes.string.isRequired,
-  };
+  value: PropTypes.number.isRequired,
+  name: PropTypes.string.isRequired,
+};
 
-  export default Value;
+export default Value;
---
+++ src/functions/orehaCalc/OrehaCalc.test.js
@@ -0,0 +1,5 @@
+import OrehaCalc from "./OrehaCalc";
+
+it("answer correctly", () => {
+  expect(OrehaCalc(1, 1, 1)).toBe(1);
+});
