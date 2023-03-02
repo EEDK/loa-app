ad0221704d9e20eebaaab4954b13e39cbe9d186f
KDE <kde9604@naver.com>
2023-03-02 12:31:54 +0900

    add test case and solve oreha calc

--- src/functions/orehaCalc/OrehaCalc.js
+++ src/functions/orehaCalc/OrehaCalc.js
@@ -1,12 +1,11 @@
 /*  오레하 계산기 정보
     normal -> 고대유물(100개)
     common(희귀유물 10개)
-    oreha(오레하 유뭂 10)
-    selector -> 어느 오레하의 제작비용인지 알아내는 구분자 0 -> 중급 1-> 상급 2-> 최상급
+    oreha(오레하 유물 10)
     discountRate -> 제작비용 할인률
 
-
-    중급 오레하 :30개 제작, 고대 유물 64, 희귀한 유물 26, 오레하 유물 8개 제작 비용 205 골드
+    하급 오레하 : 30개 제작, 고대 유물 : 56, 희귀 유물 : 28, 오레하 유물 : 7개 제작 비용 200골드
+    중급 오레하 :30개 제작, 고대 유물 64, 희귀한 유물 26, 오레하 유물 8개 제작 비용 200 골드
     상급 오레하 : 20개 제작, 고대 유물 94, 희귀한 유물 29, 오레하 유물 16개 제작 비용 250 골드
     최상급 오레하 : 15개 제작, 고대 유물 107, 희귀한 유물 51, 오레하 유물 52개 제작 비용 300 골드
 
@@ -29,20 +28,50 @@ function OrehaCalc(name, nowValue, RelicData, discountRate) {
     }
   });
 
-  console.log(name, nowValue, RelicData, discountRate);
-
   switch (name) {
     case '하급 오레하 융화 재료':
-      result = 1;
+      result =
+        (nowValue * 30 -
+          (normal * 0.56 +
+            common * 2.8 +
+            oreha * 0.7 +
+            200 * ((100 - discountRate) / 100))) /
+        30;
+
+      result = result.toFixed(2);
       break;
     case '중급 오레하 융화 재료':
-      result = 2;
+      result =
+        (nowValue * 30 -
+          (normal * 0.64 +
+            common * 2.6 +
+            oreha * 0.8 +
+            200 * ((100 - discountRate) / 100))) /
+        30;
+
+      result = result.toFixed(2);
       break;
     case '상급 오레하 융화 재료':
-      result = 3;
+      result =
+        (nowValue * 20 -
+          (normal * 0.94 +
+            common * 2.9 +
+            oreha * 1.6 +
+            250 * ((100 - discountRate) / 100))) /
+        20;
+
+      result = result.toFixed(2);
       break;
     case '최상급 오레하 융화 재료':
-      result = 4;
+      result =
+        (nowValue * 15 -
+          (normal * 1.07 +
+            common * 5.1 +
+            oreha * 5.2 +
+            300 * ((100 - discountRate) / 100))) /
+        15;
+
+      result = result.toFixed(2);
       break;
 
     default:
--- src/functions/orehaCalc/OrehaCalc.test.js
+++ src/functions/orehaCalc/OrehaCalc.test.js
@@ -1,6 +1,6 @@
 import OrehaCalc from './OrehaCalc';
 
-it('answer correctly', () => {
+it('하급 오레하 계산 정상', () => {
   expect(
     OrehaCalc(
       '하급 오레하 융화 재료',
@@ -58,5 +58,188 @@ it('answer correctly', () => {
       },
       10
     )
-  ).toBe(1);
+  ).toBe('-0.58');
+});
+
+it('중급 오레하 계산 확인', () => {
+  expect(
+    OrehaCalc(
+      '중급 오레하 융화 재료',
+      11,
+      {
+        PageNo: 1,
+        PageSize: 10,
+        TotalCount: 4,
+        Items: [
+          {
+            Id: 6882701,
+            Name: '고대 유물',
+            Grade: '일반',
+            Icon: 'https://cdn-lostark.game.onstove.com/EFUI_IconAtlas/Use/Use_9_3.png',
+            BundleCount: 100,
+            TradeRemainCount: null,
+            YDayAvgPrice: 51.4,
+            RecentPrice: 52,
+            CurrentMinPrice: 52,
+          },
+          {
+            Id: 6882704,
+            Name: '희귀한 유물',
+            Grade: '고급',
+            Icon: 'https://cdn-lostark.game.onstove.com/EFUI_IconAtlas/Use/Use_9_4.png',
+            BundleCount: 10,
+            TradeRemainCount: null,
+            YDayAvgPrice: 10.3,
+            RecentPrice: 11,
+            CurrentMinPrice: 10,
+          },
+          {
+            Id: 6884707,
+            Name: '칼다르 유물',
+            Grade: '희귀',
+            Icon: 'https://cdn-lostark.game.onstove.com/EFUI_IconAtlas/Use/Use_9_10.png',
+            BundleCount: 10,
+            TradeRemainCount: null,
+            YDayAvgPrice: 1,
+            RecentPrice: 1,
+            CurrentMinPrice: 1,
+          },
+          {
+            Id: 6885708,
+            Name: '오레하 유물',
+            Grade: '희귀',
+            Icon: 'https://cdn-lostark.game.onstove.com/EFUI_IconAtlas/Use/Use_9_11.png',
+            BundleCount: 10,
+            TradeRemainCount: null,
+            YDayAvgPrice: 66.9,
+            RecentPrice: 68,
+            CurrentMinPrice: 67,
+          },
+        ],
+      },
+      10
+    )
+  ).toBe('1.12');
+});
+
+it('상급 오레하 계산 확인', () => {
+  expect(
+    OrehaCalc(
+      '상급 오레하 융화 재료',
+      23,
+      {
+        PageNo: 1,
+        PageSize: 10,
+        TotalCount: 4,
+        Items: [
+          {
+            Id: 6882701,
+            Name: '고대 유물',
+            Grade: '일반',
+            Icon: 'https://cdn-lostark.game.onstove.com/EFUI_IconAtlas/Use/Use_9_3.png',
+            BundleCount: 100,
+            TradeRemainCount: null,
+            YDayAvgPrice: 51.4,
+            RecentPrice: 48,
+            CurrentMinPrice: 52,
+          },
+          {
+            Id: 6882704,
+            Name: '희귀한 유물',
+            Grade: '고급',
+            Icon: 'https://cdn-lostark.game.onstove.com/EFUI_IconAtlas/Use/Use_9_4.png',
+            BundleCount: 10,
+            TradeRemainCount: null,
+            YDayAvgPrice: 10.3,
+            RecentPrice: 9,
+            CurrentMinPrice: 10,
+          },
+          {
+            Id: 6884707,
+            Name: '칼다르 유물',
+            Grade: '희귀',
+            Icon: 'https://cdn-lostark.game.onstove.com/EFUI_IconAtlas/Use/Use_9_10.png',
+            BundleCount: 10,
+            TradeRemainCount: null,
+            YDayAvgPrice: 1,
+            RecentPrice: 1,
+            CurrentMinPrice: 1,
+          },
+          {
+            Id: 6885708,
+            Name: '오레하 유물',
+            Grade: '희귀',
+            Icon: 'https://cdn-lostark.game.onstove.com/EFUI_IconAtlas/Use/Use_9_11.png',
+            BundleCount: 10,
+            TradeRemainCount: null,
+            YDayAvgPrice: 66.9,
+            RecentPrice: 62,
+            CurrentMinPrice: 67,
+          },
+        ],
+      },
+      0
+    )
+  ).toBe('1.98');
+});
+
+it('최상급 오레하 계산 확인', () => {
+  expect(
+    OrehaCalc(
+      '최상급 오레하 융화 재료',
+      49,
+      {
+        PageNo: 1,
+        PageSize: 10,
+        TotalCount: 4,
+        Items: [
+          {
+            Id: 6882701,
+            Name: '고대 유물',
+            Grade: '일반',
+            Icon: 'https://cdn-lostark.game.onstove.com/EFUI_IconAtlas/Use/Use_9_3.png',
+            BundleCount: 100,
+            TradeRemainCount: null,
+            YDayAvgPrice: 51.4,
+            RecentPrice: 48,
+            CurrentMinPrice: 52,
+          },
+          {
+            Id: 6882704,
+            Name: '희귀한 유물',
+            Grade: '고급',
+            Icon: 'https://cdn-lostark.game.onstove.com/EFUI_IconAtlas/Use/Use_9_4.png',
+            BundleCount: 10,
+            TradeRemainCount: null,
+            YDayAvgPrice: 10.3,
+            RecentPrice: 9,
+            CurrentMinPrice: 10,
+          },
+          {
+            Id: 6884707,
+            Name: '칼다르 유물',
+            Grade: '희귀',
+            Icon: 'https://cdn-lostark.game.onstove.com/EFUI_IconAtlas/Use/Use_9_10.png',
+            BundleCount: 10,
+            TradeRemainCount: null,
+            YDayAvgPrice: 1,
+            RecentPrice: 1,
+            CurrentMinPrice: 1,
+          },
+          {
+            Id: 6885708,
+            Name: '오레하 유물',
+            Grade: '희귀',
+            Icon: 'https://cdn-lostark.game.onstove.com/EFUI_IconAtlas/Use/Use_9_11.png',
+            BundleCount: 10,
+            TradeRemainCount: null,
+            YDayAvgPrice: 66.9,
+            RecentPrice: 62,
+            CurrentMinPrice: 67,
+          },
+        ],
+      },
+      0
+    )
+  ).toBe('1.02');
 });
